ab0950416cbec6a2ceb21097433da11e6740b530
Riccardo Colombo <rikyregna@gmail.com>
2020-07-13 15:13:09 +0200

    algoritmo problema

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
-/* Lista Cognomi
+/* Lista Cognomi.
 Chiedere all’utente il cognome inserirlo in un array con altri cognomi:
 ‘Bianchi’, ‘Rossi’, ‘Duzioni’, ‘Balsano’, ‘Verdi’
 stampa la lista ordinata alfabeticamente scrivi anche la posizione 
 "umana" della lista in cui il nuovo utente si trova */
+
+// Acquisire il cognome tramite prompt
+// Capitalize del nome
+// Pushare il nome nell'array [‘Bianchi’, ‘Rossi’, ‘Duzioni’, ‘Balsano’, ‘Verdi’]
+// Ordinare l'array
+// Stampare a video l'array
+// Trovare l'indice dell'elemento inserito e stamparlo a video
+
+var cognome = prompt("Inserisci il tuo cognome");
+
+console.log(cognome);
