c32e6394af74bbdb64c8113a2b005dd5df9aa56d
Riccardo Colombo <rikyregna@gmail.com>
2020-07-13 15:08:20 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,5 @@
+/* Lista Cognomi
+Chiedere all’utente il cognome inserirlo in un array con altri cognomi:
+‘Bianchi’, ‘Rossi’, ‘Duzioni’, ‘Balsano’, ‘Verdi’
+stampa la lista ordinata alfabeticamente scrivi anche la posizione 
+"umana" della lista in cui il nuovo utente si trova */
